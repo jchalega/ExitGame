314f9b361bc7497c8aff1aa2d0e8e21148da6656
João Chalega <joaopathergy@gmail.com>
2026-05-15 16:23:11 -0400

    Update teste.cs

--- Assets/Scripts/teste.cs
+++ Assets/Scripts/teste.cs
@@ -2,38 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movimentacao : MonoBehaviour
-{
-    [Header("Configurações de Movimento")]
-    public float velocidade = 10f;
-
-    private Rigidbody2D rb; // Mude para Rigidbody se estiver em 3D
-    private Vector2 direcaoMovimento;
-
-    void Start()
-    {
-        // Pega o componente de física do personagem
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        // Captura as teclas pressionadas (W, A, S, D ou Setas)
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
-
-        direcaoMovimento = new Vector2(inputX, inputY).normalized;
-    }
-
-    // FixedUpdate é o mais indicado para alterar o Rigidbody
-    void FixedUpdate()
-    {
-        Andar();
-    }
-
-    void Andar()
-    {
-        // Aplica o movimento ao Rigidbody
-        rb.MovePosition(rb.position + direcaoMovimento * velocidade * Time.fixedDeltaTime);
-    }
-}
+//Isso eh apenas um teste -_-
