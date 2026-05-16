96c4401749654957b15d794447d4326718ee392c
João Chalega <joaopathergy@gmail.com>
2026-05-16 18:27:42 -0400

    Inicio da movimentação padrão do Player
    - Script "Player" criado
    - Função de andar no eixo Horizontal e Vertical
    - Animações de IDLE, ATAQUE e ANDAR

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] public float speed;
+
+    [SerializeField] private Animator animator;
+    private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;  
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rigidbody2D.linearVelocity = direction.normalized * speed;
+        if (direction != Vector2.zero)
+        {
+            animator.SetBool("isRunning", true);
+            if (direction.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (direction.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+        
+      
+        
+            
+        
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+            return;
+        }
+        
+                
+        
+    }
+}
