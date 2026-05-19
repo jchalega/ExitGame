8ad7f3fc5ccd3ef904f08bca9bf126bd1d733868
João Chalega <joaopathergy@gmail.com>
2026-05-19 12:08:39 -0400

    Tamo de Volta

--- Assets/Scripts/npc.cs
+++ Assets/Scripts/npc.cs
@@ -7,7 +7,7 @@ public class npc : MonoBehaviour
     public GameObject caixaDialogo;
     public TMP_Text textoDialogo;
 
-    [TextArea(3, 5)]
+    [TextArea(4, 6)]
     public string falaNPC;
 
     public float velocidade = 0.03f;
@@ -16,12 +16,16 @@ public class npc : MonoBehaviour
 
     private void Start()
     {
+        falaNPC = "Precisamos de mais suprimentos urgente.\nSuba as escadas e procure alguns lanches. Nao morra!!";
+
+        textoDialogo.text = "";
         caixaDialogo.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
+        Debug.Log(gameObject.name + " -> " + falaNPC);
 
         caixaDialogo.SetActive(true);
 
@@ -40,6 +44,7 @@ public class npc : MonoBehaviour
         if (animacaoTexto != null)
             StopCoroutine(animacaoTexto);
     }
+
 
     IEnumerator TypeText(string texto)
     {
