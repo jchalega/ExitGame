882529b877cad48a717ee025e7adac85d4b59987
João Chalega <joaopathergy@gmail.com>
2026-05-19 12:51:24 -0400

    Update npc.cs

--- Assets/Scripts/npc.cs
+++ Assets/Scripts/npc.cs
@@ -25,7 +25,7 @@ public class npc : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
-        Debug.Log(gameObject.name + " -> " + falaNPC);
+        
 
         caixaDialogo.SetActive(true);
 
